70537cf5abd373a23fe1e4f3720822ca8fd72e6d
alvinix <119513577+alvinix@users.noreply.github.com>
2022-12-01 14:45:28 +0700

    new_Config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
   cognito: {
-    userPoolId: "us-east-1_RoNsoYaFG",
-    userPoolClientId: "5270pqgq71gsb098gnud5r4lu2",
+    userPoolId: "us-east-1_lPe8eyUqB",
+    userPoolClientId: "2ujc0jmt4s5c9blcnj9g0l2rrq",
     region: "us-east-1"
   },
   api: {
